364651d770784e452ceb44aef9606e0e893a4841
Jiaran Hao <jiahao@oradev.oraclecorp.com>
2015-11-05 16:43:57 -0800

    create repo

---
+++ helloworld.js
@@ -0,0 +1,16 @@
+/**
+ * New node file
+ */
+
+console.log("hello world !");
+console.log('Hi:%s',"Jiaran");
+
+
+var http = require('http');
+http.createServer(function(req, res) {
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	res.write('<h1>Node.js</h1>');
+	res.end('<p>Hello World</p>');
+}).listen(3000);
+
+console.log("HTTP server is listening at port 3000.");
